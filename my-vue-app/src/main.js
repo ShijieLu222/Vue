c560bc1ec3eac574d046336733e91d4ce7eb36b3
Shijie Lu <ig23200@bristol.ac.uk>
2025-07-14 14:52:49 +0800

    add antdesign

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import router from './router';
+import Antd from 'ant-design-vue';
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.mount('#app');
+app.use(Antd); // 注册 Ant Design Vue
--- my-vue-app/src/views/Login.vue
+++ my-vue-app/src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="max-width: 300px;margin: 40px auto;">
     <h2>登录页面</h2>
     <p>请输入用户名和密码进行登录。</p>
   </div>
