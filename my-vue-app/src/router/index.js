615a3105a136742f78d135996ec2fac9420b046c
Shijie Lu <ig23200@bristol.ac.uk>
2025-07-14 14:22:55 +0800

    add routes

--- my-vue-app/src/router/index.js
+++ my-vue-app/src/router/index.js
@@ -1,9 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 const routes = [
-  // 示例页面路由
-  // { path: '/venue', component: () => import('@/views/VenueList.vue') },
-];
+  { path: '/login', component: Login },
+  { path: '/register', component: Register },
+  { path: '/dashboard', component: Dashboard }
+]
 
 const router = createRouter({
   history: createWebHistory(),
--- my-vue-app/src/views/Register.vue
+++ my-vue-app/src/views/Register.vue
@@ -1,5 +1,8 @@
 <template>
-  
+  <div>
+    <h2>注册页面</h2>
+    <p>请填写信息完成注册。</p>
+  </div>
 </template>
 
 <script>
